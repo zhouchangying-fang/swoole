d9335e119ad412af4f7a3beccabd2ff347076bbd
zhouchangying-fang <376428457@qq.com>
2020-04-07 10:31:07 +0800

    tcp

--- tcp_client.php
+++ tcp_client.php
@@ -4,8 +4,9 @@ socket_connect($socket,'127.0.0.1',9501);
 $str ="这是一个发送信息";
 socket_send($socket,$str,strlen($str),MSG_DONTROUTE);
 $buf = "";
-$false = socket_recv($socket,$buf,1,MSG_DONTWAIT);
-while (socket_recv($socket,$buf,1,MSG_DONTWAIT)){
+$false = socket_recv($socket,$buf,2048,MSG_DONTWAIT);
+/*while (socket_recv($socket,$buf,1,MSG_DONTWAIT)){
     echo $buf.'\n';
-}
+}*/
+echo $buf;
 socket_close($socket);
